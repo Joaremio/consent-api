f8d187f5ca706b38a73246a19fe1400a8b678fae
Joaremio <joaremiorevoredo@gmail.com>
2026-03-31 23:55:47 -0300

    feat(consent): implementar endpoints REST para gerenciamento de consentimentos

---
+++ src/main/java/br/com/sensedia/controller/ConsentController.java
@@ -0,0 +1,60 @@
+package br.com.sensedia.controller;
+
+import br.com.sensedia.dto.ConsentRequestDTO;
+import br.com.sensedia.dto.ConsentResponseDTO;
+import br.com.sensedia.service.ConsentService;
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/consents")
+public class ConsentController {
+
+    private final ConsentService consentService;
+
+    public ConsentController(ConsentService consentService) {
+        this.consentService = consentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ConsentResponseDTO> createConsent(
+            @RequestHeader("X-Idempotency-Key") String idempotencyKey,
+            @Valid @RequestBody ConsentRequestDTO data) {
+
+        ConsentResponseDTO response = consentService.createConsent(data, idempotencyKey);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("/{consentId}")
+    public ResponseEntity<ConsentResponseDTO> getConsentById(@PathVariable  UUID consentId) {
+        return ResponseEntity.status(HttpStatus.OK).body(consentService.getConsentById(consentId));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ConsentResponseDTO>> getAllConsents(Pageable pageable) {
+        return ResponseEntity.status(HttpStatus.OK).body(consentService.getAllConsents(pageable));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ConsentResponseDTO> update(
+            @PathVariable UUID id,
+            @Valid @RequestBody ConsentRequestDTO dto) {
+        return ResponseEntity.ok(consentService.updateConsent(id, dto));
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void revoke(@PathVariable UUID id) {
+        consentService.revokeConsent(id);
+    }
+
+
+}
